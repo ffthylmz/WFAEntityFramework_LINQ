0bcd4ab7ac9e28c77dec8ab06c24c77fc9237e67
ffthylmz <ffthylmz@outlook.com>
2022-08-27 19:18:18 +0300

    Add project files.

---
+++ WFAEntity270822G/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WFAEntity270822G
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgwList = new System.Windows.Forms.DataGridView();
+            this.btnSorgu1 = new System.Windows.Forms.Button();
+            this.btnSorgu2 = new System.Windows.Forms.Button();
+            this.btnSorgu3 = new System.Windows.Forms.Button();
+            this.btnSorgu6 = new System.Windows.Forms.Button();
+            this.btnSorgu5 = new System.Windows.Forms.Button();
+            this.btnSorgu4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgwList
+            // 
+            this.dgwList.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.dgwList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwList.Location = new System.Drawing.Point(12, 138);
+            this.dgwList.Name = "dgwList";
+            this.dgwList.Size = new System.Drawing.Size(776, 300);
+            this.dgwList.TabIndex = 0;
+            // 
+            // btnSorgu1
+            // 
+            this.btnSorgu1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnSorgu1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnSorgu1.Location = new System.Drawing.Point(73, 55);
+            this.btnSorgu1.Name = "btnSorgu1";
+            this.btnSorgu1.Size = new System.Drawing.Size(75, 23);
+            this.btnSorgu1.TabIndex = 1;
+            this.btnSorgu1.Text = "Sorgu1";
+            this.btnSorgu1.UseVisualStyleBackColor = false;
+            this.btnSorgu1.Click += new System.EventHandler(this.btnSorgu1_Click);
+            // 
+            // btnSorgu2
+            // 
+            this.btnSorgu2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnSorgu2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnSorgu2.Location = new System.Drawing.Point(177, 55);
+            this.btnSorgu2.Name = "btnSorgu2";
+            this.btnSorgu2.Size = new System.Drawing.Size(75, 23);
+            this.btnSorgu2.TabIndex = 2;
+            this.btnSorgu2.Text = "Sorgu2";
+            this.btnSorgu2.UseVisualStyleBackColor = false;
+            this.btnSorgu2.Click += new System.EventHandler(this.btnSorgu2_Click);
+            // 
+            // btnSorgu3
+            // 
+            this.btnSorgu3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnSorgu3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnSorgu3.Location = new System.Drawing.Point(287, 55);
+            this.btnSorgu3.Name = "btnSorgu3";
+            this.btnSorgu3.Size = new System.Drawing.Size(75, 23);
+            this.btnSorgu3.TabIndex = 3;
+            this.btnSorgu3.Text = "Sorgu3";
+            this.btnSorgu3.UseVisualStyleBackColor = false;
+            this.btnSorgu3.Click += new System.EventHandler(this.btnSorgu3_Click);
+            // 
+            // btnSorgu6
+            // 
+            this.btnSorgu6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnSorgu6.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnSorgu6.Location = new System.Drawing.Point(607, 55);
+            this.btnSorgu6.Name = "btnSorgu6";
+            this.btnSorgu6.Size = new System.Drawing.Size(75, 23);
+            this.btnSorgu6.TabIndex = 6;
+            this.btnSorgu6.Text = "Sorgu6";
+            this.btnSorgu6.UseVisualStyleBackColor = false;
+            this.btnSorgu6.Click += new System.EventHandler(this.btnSorgu6_Click);
+            // 
+            // btnSorgu5
+            // 
+            this.btnSorgu5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnSorgu5.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnSorgu5.Location = new System.Drawing.Point(497, 55);
+            this.btnSorgu5.Name = "btnSorgu5";
+            this.btnSorgu5.Size = new System.Drawing.Size(75, 23);
+            this.btnSorgu5.TabIndex = 5;
+            this.btnSorgu5.Text = "Sorgu5";
+            this.btnSorgu5.UseVisualStyleBackColor = false;
+            this.btnSorgu5.Click += new System.EventHandler(this.btnSorgu5_Click);
+            // 
+            // btnSorgu4
+            // 
+            this.btnSorgu4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnSorgu4.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnSorgu4.Location = new System.Drawing.Point(393, 55);
+            this.btnSorgu4.Name = "btnSorgu4";
+            this.btnSorgu4.Size = new System.Drawing.Size(75, 23);
+            this.btnSorgu4.TabIndex = 4;
+            this.btnSorgu4.Text = "Sorgu4";
+            this.btnSorgu4.UseVisualStyleBackColor = false;
+            this.btnSorgu4.Click += new System.EventHandler(this.btnSorgu4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSorgu6);
+            this.Controls.Add(this.btnSorgu5);
+            this.Controls.Add(this.btnSorgu4);
+            this.Controls.Add(this.btnSorgu3);
+            this.Controls.Add(this.btnSorgu2);
+            this.Controls.Add(this.btnSorgu1);
+            this.Controls.Add(this.dgwList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgwList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgwList;
+        private System.Windows.Forms.Button btnSorgu1;
+        private System.Windows.Forms.Button btnSorgu2;
+        private System.Windows.Forms.Button btnSorgu3;
+        private System.Windows.Forms.Button btnSorgu6;
+        private System.Windows.Forms.Button btnSorgu5;
+        private System.Windows.Forms.Button btnSorgu4;
+    }
+}
+
---
+++ WFAEntity270822G/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.SqlServer;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAEntity270822G
+{
+    public partial class Form1 : Form
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnSorgu1_Click(object sender, EventArgs e)
+        {
+            //Çalışanların isim soyisim ünvan doğumtarihi bilgilerini getiriniz.
+
+            #region LinqToEntity
+
+            //dgwList.DataSource = db.Employees.Select(a => new
+            //{
+            //    İsim = a.FirstName,
+            //    Soyisim = a.LastName,
+            //    Ünvan = a.Title,
+            //    DoğumTarihi = a.BirthDate
+            //}).ToList();
+
+            #endregion
+
+            #region LinqToSQL
+
+            //SELECT FirstName, LastName, Title, BirthDate FROM Employees
+
+            var result = from emp in db.Employees
+                         select new
+                         {
+                             İsim = emp.FirstName,
+                             Soyisim = emp.LastName,
+                             Ünvan = emp.Title,
+                             DoğumTarihi = emp.BirthDate
+                         };
+
+            dgwList.DataSource = result.ToList();
+
+            #endregion
+        }
+
+        private void btnSorgu2_Click(object sender, EventArgs e)
+        {
+            //Çalışanların ID'si 2 ve 8 arasında olanların A-Z sıralanacak şekilde ID,AD,SOYAD,UNVAN bilgilerini listeleyiniz.
+
+            #region LinqToEntity
+
+            //dgwList.DataSource = db.Employees.Select(a => new
+            //{
+            //    ID = a.EmployeeID,
+            //    İsim = a.FirstName,
+            //    Soyisim = a.LastName,
+            //    Ünvan = a.Title
+            //}).Where(a => a.ID >= 2 && a.ID < 8).OrderBy(z => z.İsim).ToList();
+
+
+            #endregion
+
+            #region LinqToSQL
+
+            var result = from emp in db.Employees
+                         where emp.EmployeeID >= 2 && emp.EmployeeID < 8
+                         orderby emp.FirstName
+                         select new
+                         {
+                             ID = emp.EmployeeID,
+                             İsim = emp.FirstName,
+                             Soyisim = emp.LastName,
+                             Ünvan = emp.Title
+                         };
+
+            dgwList.DataSource = result.ToList();
+
+            #endregion
+        }
+
+        private void btnSorgu3_Click(object sender, EventArgs e)
+        {
+            //1960 yılında doğan çalışanların adı, soyadı, doğum tarihini getiriniz.
+
+            #region LinqToEntity
+
+            //1.YOL
+
+            //dgwList.DataSource = db.Employees.Select(a => new
+            //{
+            //    İsim = a.FirstName,
+            //    Soyisim = a.LastName,
+            //    DogumTarihi = a.BirthDate
+            //}).Where(a => a.DogumTarihi.Value.Year == 1960).ToList();
+
+            //2.YOL
+
+            //dgwList.DataSource = db.Employees.Select(a => new
+            //{
+            //    İsim = a.FirstName,
+            //    Soyisim = a.LastName,
+            //    DogumTarihi = a.BirthDate
+            //}).Where(a => a.DogumTarihi.Value.Equals(1960)).ToList();
+
+            //3.YOL
+
+            //dgwList.DataSource = db.Employees.Select(a => new
+            //{
+            //    İsim = a.FirstName,
+            //    Soyisim = a.LastName,
+            //    DogumTarihi = a.BirthDate
+            //}).Where(a => SqlFunctions.DatePart("YEAR", a.DogumTarihi) == 1960).ToList();
+
+
+            #endregion
+
+            #region LinqToSQL
+
+            var result = from emp in db.Employees
+                         where emp.BirthDate.Value.Year == 1960
+                         select new
+                         {
+
+                             İsim = emp.FirstName,
+                             Soyisim = emp.LastName,
+                             DogumTarihi = emp.BirthDate
+                         };
+
+            dgwList.DataSource = result.ToList();
+
+            #endregion
+        }
+
+        private void btnSorgu4_Click(object sender, EventArgs e)
+        {
+            //60 yaşından büyük olan çalışanların Adı, Soyadını , Doğum tarihini, A'dan Z'ye sıralayınız bilgilerini getiren linq to entity ve linq to sql sorgularını yazınız
+
+            #region LinqToEntity
+
+            //dgwList.DataSource = db.Employees.Select(a => new
+            //{
+            //    a.FirstName,
+            //    a.LastName,
+            //    a.BirthDate,
+            //    Yas = SqlFunctions.DateDiff("YEAR", a.BirthDate, DateTime.Now)
+            //}).Where(a => SqlFunctions.DateDiff("YEAR", a.BirthDate, DateTime.Now) > 60).OrderBy(a => a.FirstName).ToList();
+
+
+            #endregion
+
+            #region LinqToSQL
+
+            var result = from emp in db.Employees
+                         where SqlFunctions.DateDiff("YEAR", emp.BirthDate, DateTime.Now) > 60
+                         select new
+                         {
+                             İsim = emp.FirstName,
+                             Soyisim = emp.LastName,
+                             DogumTarihi = emp.BirthDate,
+                             Yas = SqlFunctions.DateDiff("YEAR", emp.BirthDate, DateTime.Now)
+                         };
+
+            dgwList.DataSource = result.ToList();
+
+            #endregion
+
+        }
+
+        private void btnSorgu5_Click(object sender, EventArgs e)
+        {
+            //Doğum tarihi 1930 ile 1960 arasında olan ve USA'da oturan çalışanların bilgilerini getiren linq to entity ve linq to sql sorgularını yazınız.
+
+            #region LinqToEntity
+
+            dgwList.DataSource = db.Employees.Where(a => a.BirthDate.Value.Year > 1930 && a.BirthDate.Value.Year < 1960 && a.Country == "USA").ToList();
+
+
+            #endregion
+
+            #region LinqToSQL
+
+            var result = from emp in db.Employees
+                         where (emp.BirthDate.Value.Year > 1930 && emp.BirthDate.Value.Year < 1960 && emp.Country == "USA")
+                         select emp;
+
+            dgwList.DataSource = result.ToList();
+
+            #endregion
+        }
+
+        private void btnSorgu6_Click(object sender, EventArgs e)
+        {
+            //Kategorilerime göre stok durum nasıl? bilgisini getiren linq to entity ve linq to sql sorgularını yazınız.
+
+            #region LinqToEntity
+
+            //dgwList.DataSource = db.Products.GroupBy(a => a.Category.CategoryName).Select(b => new
+            //{
+            //    b.Key,
+            //    ToplamStok = b.Sum(c => c.UnitsInStock)
+            //}).ToList();
+
+
+            #endregion
+
+            #region LinqToSQL
+
+            var result = from prod in db.Products
+                         group prod by prod.Category.CategoryName into g
+                         select new
+                         {
+                             KategoriAdi = g.Key,
+                             ToplamStok = g.Sum(c => c.UnitsInStock)
+                         };
+
+            dgwList.DataSource = result.ToList();
+
+            #endregion
+        }
+    }
+}
